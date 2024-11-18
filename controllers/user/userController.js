629e405065c3bca08445d80942f00d9a637df5ff
Anandhu Sreepadam <anandhusreepadam@gmail.com>
2024-11-18 12:53:51 +0530

    view engine setup

---
+++ controllers/user/userController.js
@@ -0,0 +1,29 @@
+
+
+const pageNotFound = async(req,res)=>{
+    try {
+        res.render('page-404')
+    } catch (error) {
+        res.redirect('/pageNotFound')
+    }
+}
+
+
+
+
+
+const loadHomepage = async(req,res)=>{
+    try {
+        return res.render('home');
+    } catch (error) {
+        console.log('Home Page not found');
+        res.status(500).send('Server Error');
+    }
+}
+
+
+
+module.exports = {
+    loadHomepage,
+    pageNotFound
+};
