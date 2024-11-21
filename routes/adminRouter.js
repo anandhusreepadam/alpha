6ea34569519f883b2ea5d3d4cfce7cc5b2c74e11
Anandhu Sreepadam <anandhusreepadam@gmail.com>
2024-11-21 11:06:50 +0530

    category offer implemented

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminController = require('../controllers/admin/adminController');
 const {userAtuth,adminAuth}=require('../middlewares/auth');
 const customerController= require('../controllers/admin/customerController');
+const categoryController= require('../controllers/admin/categoryController');
 
 
 
@@ -16,7 +17,11 @@ router.get('/users',adminAuth,customerController.customerInfo);
 router.get('/blockCustomer',adminAuth,customerController.customerBlocked);
 router.get('/unBlockCustomer',adminAuth,customerController.customerUnblocked);
 
+router.get('/category',adminAuth,categoryController.categoryInfo);
+router.post('/category',adminAuth,categoryController.addCategory);
 
+router.post('/addCategoryOffer',adminAuth,categoryController.addCategoryOffer);
+router.post('/removeCategoryOffer',adminAuth,categoryController.removeCategoryOffer)
 
 
 
