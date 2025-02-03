ebad46f34ad11c3d201a084e0a25d8a0caf5de1b
ANANDHU S <anandhusreepadam@gmail.com>
2025-02-03 10:16:52 +0530

    Update multer.js

--- helpers/multer.js
+++ helpers/multer.js
@@ -2,17 +2,13 @@ const { fileLoader } = require('ejs');
 const multer = require('multer');
 const path = require('path');
 
-
 const storage = multer.diskStorage({
-
     destination:(req,res,cb)=>{
         cb(null,path.join(__dirname,"../public/uploads/re-image"));
-
     },
     filename:(req,res,cb)=>{
         cb(null,Date.now()+".png");
     }
-
 })
 
 
